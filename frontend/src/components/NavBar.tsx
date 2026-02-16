e832f0b185f3338c68d76ae7aed00cdf3705afa3
Justin Lew <justinlew66@gmail.com>
2026-02-16 19:16:29 +1100

    .

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+export default function NavBar() {
+    return (
+        <nav className="bg-red-200 p-6 w-full h-16 flex items-center justify-between">
+            <p>Fooood :D</p>
+            <div className="flex gap-6">
+                <p>Sign Up</p>
+                <p>Login</p>
+            </div>
+        </nav>
+    )
+}
