d16a1c26d835cc371404b27a9e19e79246f24eed
Justin Lew <justinlew66@gmail.com>
2026-02-20 21:52:45 +1100

    Auth middleware

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,18 @@
-use axum::{Router, routing::get};
+use std::sync::Arc;
+
+use axum::{Router, middleware, routing::get};
+use jsonwebtoken::jwk::{JwkSet};
 use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
 use tracing::{Level, info};
+
+use crate::middlewares::auth::auth_guard;
+
+mod middlewares;
 
 #[derive(Clone)]
 struct AppState {
     db: Pool<Postgres>,
+    jwks: JwkSet,
 }
 
 #[tokio::main]
@@ -22,13 +30,20 @@ async fn main() {
         .connect(&database_url)
         .await
         .expect("Failed to create pool");
-    let state = AppState {
+
+    let jwks = reqwest::get("https://yqcaszfogwfzlbbzhlrz.supabase.co/auth/v1/.well-known/jwks.json")
+    .await.expect("Failed to fetch JWKS").json().await.expect("Failed to parse JWKS");
+    let state = Arc::new(AppState {
         db: pool,
-    };
+        jwks: jwks
+    });
     info!("Connected to Database...");
 
     let app = Router::new()
     .route("/api/test", get(|| async { "Hello World"}))
+    .route("/api/protected", get(|| async {"Protected Route"}).layer(
+        middleware::from_fn_with_state(state.clone(), auth_guard)
+    ))
     .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:6000").await.unwrap();
---
+++ backend/src/middlewares/auth.rs
@@ -0,0 +1,62 @@
+use std::sync::Arc;
+
+use axum::{extract::{Request, State}, http::{StatusCode, response}, middleware::Next, response::{IntoResponse, Response}};
+use jsonwebtoken::{Algorithm, DecodingKey, Validation, decode, decode_header};
+use serde::Deserialize;
+
+use crate::AppState;
+
+#[derive(Deserialize, Clone)]
+struct Claims {
+    iss: String,
+    sub: String,
+    exp: i64,
+    role: String,
+}
+
+pub async fn auth_guard(State(state): State<Arc<AppState>>, mut req: Request, next: Next) -> Response {
+    let token_str = req.headers()
+    .get("Authorization")
+    .and_then(|header| header.to_str().ok())
+    .and_then(|header| header.strip_prefix("Bearer "));
+
+    let token = match token_str {
+        Some(t) => t,
+        None => return (StatusCode::UNAUTHORIZED, "Missing or invalid token").into_response(),
+    };
+
+    let header = match decode_header(token) {
+        Ok(h) => h,
+        Err(_) => return (StatusCode::BAD_REQUEST, "Invalid token header").into_response(),
+    };
+    let kid = match header.kid {
+        Some(k) => k,
+        None => return (StatusCode::UNAUTHORIZED, "Invalid token header").into_response(),
+    };
+
+    // TODO: Implement refreshing JWKS cache if kid is not found
+    let jwk = match state.jwks.find(&kid) {
+        Some(j) => j,
+        None => return (StatusCode::UNAUTHORIZED, "JWK not found for token").into_response(),
+    };
+
+    let mut validation = Validation::new(Algorithm::ES256);
+    validation.set_audience(&["authenticated"]);
+    // validation.set_issuer(&[""]);
+    let decoding_key = match DecodingKey::from_jwk(jwk) {
+        Ok(key) => key,
+        Err(_) => return (StatusCode::INTERNAL_SERVER_ERROR, "Failed to decode JWK").into_response(),
+    };
+
+
+    match decode::<Claims>(token, &decoding_key, &validation) {
+        Ok(token) => {
+            req.extensions_mut().insert(token.claims);
+            let response = next.run(req).await;
+            return response;
+        }
+        Err(e) => {
+            return (StatusCode::UNAUTHORIZED, "Invalid token").into_response();
+        }
+    }
+}
