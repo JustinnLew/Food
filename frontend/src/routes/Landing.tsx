3cdd7377d370bd3378453ef0b7997f2dc7e23014
Justin Lew <justinlew66@gmail.com>
2026-02-23 13:06:08 +1100

    Temporary testing for protected api route

--- frontend/src/routes/Landing.tsx
+++ frontend/src/routes/Landing.tsx
@@ -1,9 +1,28 @@
+import { useSession } from "../auth/SessionContext";
 import NavBarLanding from "../components/NavBarLanding";
 
 export default function Landing() {
+  const { session } = useSession();
+
+const handleClick = async () => {
+  const response = await fetch("http://127.0.0.1:3000/api/protected",
+    {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${session?.access_token}`
+      }
+    }
+  );
+  const data = await response.text();
+  console.log(data);
+  console.log(session?.access_token);
+}
   return (
     <div className="flex flex-col">
       <NavBarLanding />
+      <button onClick={handleClick} className="border border-red-500">
+        TEST API
+      </button>
     </div>
   );
 }
